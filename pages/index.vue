0c26379d7f0cbbf86092ba545f46b4694f6d28f5
GoobleSquibbins <arsyaarsya2005@gmail.com>
2023-05-22 13:12:44 +0700

    deleted:    components/Kard.vue 	deleted:    components/moreGarbage.vue 	deleted:    layouts/default.vue 	deleted:    pages/about.vue 	deleted:    pages/creators/[creatorId].vue 	modified:   pages/index.vue 	deleted:    pages/settingu.vue 	deleted:    pages/signin.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,198 +1,78 @@
 <template>
     <div>
-        <div class="hugeimg flex flex-col items-center ml-[150px]">
-            <img class=" h-[700px]"
-                src="https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2021/12/19BR_BattlePass_Social_Social.jpg?resize=1920%2C1080&ssl=1"
-                alt="">
-        </div>
-
-        <div class="bruhtext mr-[10px] ml-[160px] text-justify">
-            <h1 class="text-[45px] text-left border-b-2 border-[#152856] font-medium mb-[10px]">Why Fortnite Wins</h1>
-            <div class="funiimg flex flex-row-reverse items-start">
-                <img class="h-[200px] shadow-xl" src="https://media.tenor.com/cuwj6gJLLW8AAAAd/victory-royale-winner.gif"
-                    alt="">
-                <p class="indent-10 mr-[10px]">
-                    Fortnite is a free to play battle royale game published by Epic Games. Since it's release in 2017, the
-                    game
-                    has since
-                    gained massive popularity.
-                    Fortnite has been domminating the battle royale market for quiet a while now, but why ? why is Fortnite
-                    so
-                    succesful compared to it's competitor
-                    like PUBG or DAYZ, how does Fortnite thrive while PUBG and the others failed, is this all just part of a
-                    really complicated plot by the shadow government ?
-                    is freewill even real ? To find the answer to these burning question we have to go back int time, back
-                    to
-                    the dark ages of battle royales.
-                </p>
+        <h1 class="text-[50px] text-center">Diskon</h1>
+        <div class="main flex flex-col items-center w-full">
+            <div class="daform flex flex-col items-center justify-center m-auto w-[100%]">
+                <form @submit.prevent="idkmanplswork">
+                    <label class=" font-medium ">Total Pembelian</label>
+                    <br>
+                    <input type="number" required v-model="totalHarga" class="rounded border-[1px] border-black w-[200px]">
+                    <br>
+                    <br>
+                    <button class="border-[1px] border-black p-[2px] w-[200px] rounded">Submit</button>
+                </form>
             </div>
-
-            <br>
-            <ul class="list-none">
-                <li>
-                    <h1 class="text-[25px] font-medium underline">Backstory</h1>
-                    <div class="funiimg flex items-start">
-                        <img class="h-[200px] mr-[10px] shadow-xl"
-                            src="http://img0.joyreactor.com/pics/post/games-funny-gif-arma-3-1043794.gif" alt="">
-                        <p class="indent-[10px]">
-                            The year is 2017, the battle royale genre purely consist of games that are so low in quality
-                            that
-                            they were almost unplayable. Games such as h1z1 and Ark are some of the example. Yet despite all
-                            of the game's problems, the game still manage to pull morbillions of players. This proves that
-                            if
-                            someone were to make a batlle royale game and didn't completely mess it up, they could probably
-                            get
-                            alot of money. And with that we get PUBG.
-                        </p>
-                    </div>
-
-                </li>
-                <br>
-                <li>
-                    <h1 class="text-[25px] font-medium underline">PUBG</h1>
-                    <div class="letext flex flex-row-reverse items-start ">
-                        <img class="h-[200px] shadow-xl"
-                            src="https://thumbs.gfycat.com/BiodegradableSkeletalCottontail.webp" alt="">
-                        <p class="indent-[10px] mr-[10px]">
-                            PUBG emerges, the sale skyrocketed, and before you know it people spends up to $2000 dollars to
-                            buy digital drips. So, what did PUBG do that other games didn't ? nothing.
-                            turns out it did exactly what the previous games did, only slightly better. And fortunately
-                            for Brendan Green, that's just good enough to be deemed acceptable for the average gamer. Well
-                            let's see what PUBG got right and what they didn't.
-                        </p>
-                    </div>
-                    <ul class="ml-[20px]">
-                        <li>
-                            <h1 class="text-[15px] font-medium underline">Visual</h1>
-                            <div class="moretext flex items-center">
-                                <img class="h-[100px] mr-[10px] shadow-xl"
-                                    src="https://i.ytimg.com/vi/u5R0H_pysFQ/maxresdefault.jpg" alt="">
-                                <p class="indent-10">
-                                    Was PUBG nice to look at ? not really. But considering how other games looks
-                                    at the time, you couldn't really complain. Besides the game was labelled
-                                    early access, so i'm not even legally allowed to criticize it for looking bland.
-                                </p>
-                            </div>
-                        </li>
-                        <br>
-                        <li>
-                            <h1 class="text-[15px] font-medium underline">Gameplay</h1>
-                            <div class="moretext flex flex-row-reverse items-center">
-                                <img class="h-[100px] ml-[10px] shadow-xl"
-                                    src="https://images2.minutemediacdn.com/image/upload/c_fill,w_1080,ar_16:9,f_auto,q_auto,g_auto/shape%2Fcover%2Fsport%2Fpubg-06-89a3f1c559cf23f92d1ff0ffc3cbf7f2.jpg"
-                                    alt="">
-                                <p class="indent-10">
-                                    As we all know PUBG is a battle royale. Meaning, you can do anything you want, as long
-                                    as you stay in the circle. For example, sitting completely stil with a shotgun, laying
-                                    on a hill, or drive around like a maniac. When you load up a game of PUBG you find
-                                    yourself surrounded by 100 players. But what happened if you combine an unstable game
-                                    engine with 100 players ? well, you get numerous bugs and glitches that just outright
-                                    break the game. But after enough people dies the game runs just fine.
-                                </p>
-                            </div>
-                        </li>
-                        <br>
-                        <li>
-                            <h1 class="text-[15px] font-medium underline">Ballistic & Gunplay</h1>
-                            <div class="moretext flex items-center">
-                                <img class="h-[100px] mr-[10px] shadow-xl"
-                                    src="https://media.dragonblogger.com/wp-content/uploads/2022/02/15001349/Picture2-1536x864.png"
-                                    alt="">
-                                <p class="indent-10">
-                                    In terms of ballistics and gunplay, PUBG went for something more realistic, except air
-                                    isn't real.
-                                    And the only thing affecting bullet trajectory is gravity. So forget everything you
-                                    thought you know
-                                    about assault rifles, because they now have an effective range of whatever. Basically if
-                                    you put a 15x
-                                    scope on a m249 machine gun and them go prone to apply the bipod, you now have a fully
-                                    automatic, low
-                                    recoil sniper with 100 rounds per magazine.
-                                </p>
-                            </div>
-                        </li>
-                        <br>
-                        <li>
-                            <h1 class="text-[15px] font-medium underline">Audio</h1>
-                            <div class="moretext flex flex-row-reverse items-start">
-                                <img class="h-[100px] ml-[10px] shadow-xl"
-                                    src="https://quantumshifting.files.wordpress.com/2012/08/red-pill-blue-pill.jpg" alt="">
-                                <p class="indent-10">
-                                    We've reached the point of no return and you now need to make a choice. You take the
-                                    blue pill, remain
-                                    ignorant of your surroundings and get sent back to lobby because you couldn't hear the
-                                    guy sneaking up
-                                    behind you. You take the red pill, enhance your senses and become one with your
-                                    surroundings, so that no
-                                    one may get close without you knowing it. However, if you choose the red pill, you will
-                                    soon come to realize
-                                    that there is a price that you have to pay. Your hearing and tinnitus by the age of 20.
-                                    The game also had proximty
-                                    voice chat, but Blue Hole disabled it in lobbies when they found out that it was only
-                                    used to mic spamming streamers
-                                    saying the n-word, and as a marketing tool for hordes of chinese bots.
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-                <br>
-                <li>
-                    <h1 class="text-[25px] font-medium underline">PUBG's Conclusion</h1>
-                    <div class="letext flex flex-row-reverse items-start border-b-2 border-[#152856] pb-[15px]">
-                        <div class="flex flex-col items-center">
-                            <p class="indent-[10px] mr-[10px]">
-                                But in the end, PUBG was fun and i had alot of fun playing it and to be honest, i don't even
-                                notice most of
-                                these issues until they were drastically improved in you guessed it.
-                            </p>
-                            <div class="flex items-center">
-                                <img class="h-[170px] m-[10px]"
-                                    src="https://thumbs.gfycat.com/BronzeUnripeBlackrussianterrier.webp" alt="">
-                                <img class="h-[170px] m-[10px]"
-                                    src="https://media.tenor.com/F1gWP60UTpYAAAAS/fortnite-meme.gif" alt="">
-                                <h1 class="text-[50px]">F O R T N I T E</h1>
-                                <img class="h-[170px] m-[10px]"
-                                    src="https://media.tenor.com/dEjdaRZExUQAAAAd/fortnite-dance.gif" alt="">
-                                <img class="h-[170px] m-[10px]"
-                                    src="https://gifdb.com/images/high/fortnite-battle-royale-lore-explaining-meme-gwiejik8rftmq5wd.webp"
-                                    alt="">
-                            </div>
-                            <!-- ddd -->
-                        </div>
-
-                    </div>
-                </li>
-            </ul>
-
-            <div class="cards flex flex-col items-center">
-                <Kard :image="'https://www.youtube.com/embed/UnmBHkMta-4'" />
-                <div class="texd m-[10px] border-b-2 border-solid border-[#152856] w-[90%] text-center">他のクリエイター</div>
-                <div class="flex items-center">
-                    <NuxtLink to="/creators/SsethTzentach">
-                        <MoreGarbage :title="'SsethTzentach'"
-                            :image="'https://scontent.fcgk8-2.fna.fbcdn.net/v/t39.30808-1/297893906_602139911272037_100711993481514336_n.jpg?stp=dst-jpg_p200x200&_nc_cat=107&ccb=1-7&_nc_sid=c6021c&_nc_ohc=jDLXEuh7NPUAX-WWTVy&_nc_ht=scontent.fcgk8-2.fna&oh=00_AfBMud1_eDiHOOlWq2wEADjIiXCz2JS48x8UYqX9cAILWw&oe=6467D3C3'" />
-                    </NuxtLink>
-                    <NuxtLink to="/creators/Albino">
-                        <MoreGarbage :title="'AlbinoVEVO'"
-                            :image="'https://yt3.googleusercontent.com/ytc/AGIKgqMRhc2T1qMdPElVV4VA0R7nZTDaI53-6OA9nDPreg=s176-c-k-c0x00ffffff-no-rj'" />
-                    </NuxtLink>
-                    <NuxtLink to="/creators/Max0r">
-                        <MoreGarbage :title="'Max0r'"
-                            :image="'https://yt3.googleusercontent.com/ytc/AGIKgqPi0LioT8O_Xx4xs6dJPhm6CXWYaqBy8JVJvHdbQQ=s176-c-k-c0x00ffffff-no-rj'" />
-                    </NuxtLink>
-                    <NuxtLink to="/creators/Huggbees">
-                        <MoreGarbage :title="'Huggbees'"
-                            :image="'https://yt3.googleusercontent.com/ytc/AGIKgqMh5Lv0jAxa48F0jcvqw9oAfvj00WywPgGCeWS4eg=s176-c-k-c0x00ffffff-no-rj'" />
-                    </NuxtLink>
-                </div>
+            <div class="out flex flex-col items-start mt-[10px]" v-if="x == ''">
+                <table>
+                    <tr>
+                        <td>Total Pembelian</td>
+                        <td> : </td>
+                        <td>{{ totalHarga }}</td>
+                    </tr>
+                    <tr>
+                        <td>Diskon</td>
+                        <td> : </td>
+                        <td>{{ diskon }}</td>
+                    </tr>
+                    <tr>
+                        <td>Harga Final</td>
+                        <td> : </td>
+                        <td>{{ abisKenaDiskon }}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="out2">
+                <p>{{ x }}</p>
             </div>
         </div>
+
     </div>
 </template>
 
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            totalHarga: '',
+            abisKenaDiskon: '',
+            diskon: '',
+            x: '',
+        }
+    },
+    methods: {
+        idkmanplswork() {
+            if (this.totalHarga >= 100000) {
+                this.diskon = '10%'
+                this.abisKenaDiskon = this.totalHarga - (this.totalHarga * 0.1)
+            }
 
+            if (this.totalHarga >= 50000 && this.totalHarga < 100000) {
+                this.diskon = '5%'
+                this.abisKenaDiskon = this.totalHarga - (this.totalHarga * 0.05)
+            }
+
+            if (this.totalHarga < 50000) {
+                this.diskon = 'Tidak ada diskon'
+                this.abisKenaDiskon = this.totalHarga
+            }
+
+            if (this.totalHarga < 0) {
+                this.x = 'Invalid Input'
+            }
+
+        }
+    }
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
